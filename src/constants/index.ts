b48a66c1a2c25db9d795e26f127fcd1cf49ad6bd
saratovkin <svrvtwkxxn@gmail.com>
2023-01-14 01:05:58 +0100

    feat: add new links constants

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,5 @@
 import { default as PATH } from './path';
 import { default as GENRES } from './genres';
+import { default as LINKS } from './links';
 
-export { PATH, GENRES };
+export { PATH, GENRES, LINKS };
---
+++ src/constants/links.ts
@@ -0,0 +1,26 @@
+import PATH from './path';
+
+const LINKS = [
+  {
+    to: PATH.home,
+    title: 'Домой',
+  },
+  {
+    to: PATH.artistQuiz,
+    title: 'Художники',
+  },
+  {
+    to: PATH.picturesQuiz,
+    title: 'Картины',
+  },
+  {
+    to: PATH.blitzQuiz,
+    title: 'Блитц',
+  },
+  {
+    to: PATH.settings,
+    title: 'Настройки',
+  },
+];
+
+export default LINKS;
