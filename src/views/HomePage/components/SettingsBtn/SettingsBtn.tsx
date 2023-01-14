1fa86d87ab55ac97241334ed2438cc14695cdbab
saratovkin <svrvtwkxxn@gmail.com>
2023-01-14 01:06:54 +0100

    fix: remove unused component

--- src/views/HomePage/components/SettingsBtn/SettingsBtn.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import { Link } from 'react-router-dom';
-import { PATH } from '../../../../constants';
-import './SettingsBtn';
-
-const SettingsBtn = () => <Link to={PATH.settings}>Settings</Link>;
-
-export default SettingsBtn;
