d371cb70e9b84ffdaccc238a8a377f2b694ac88c
saratovkin <svrvtwkxxn@gmail.com>
2023-01-14 01:04:47 +0100

    feat: update header and footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,7 +3,14 @@ import React from 'react';
 import './Footer.css';
 
 const Footer = () => {
-  return <div className="footer">FOOTER</div>;
+  return (
+    <div className="footer">
+      <span>2023</span>
+      <a className="footer-link" href="https://github.com/saratovkin">
+        github
+      </a>
+    </div>
+  );
 };
 
 export default Footer;
---
+++ src/components/Header/components/index.ts
@@ -0,0 +1,3 @@
+import { default as NavigationBar } from './NavigationBar';
+
+export { NavigationBar };
