073f88bb3ec9ee915b36e23de6eab46a70548f70
saratovkin <svrvtwkxxn@gmail.com>
2023-01-25 19:26:28 +0100

    feat: add storage helper

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,5 +1,6 @@
 import { getRandomAnswers } from './getRandomAnswers';
 import { shuffleArray } from './shuffleArray';
 import { loadImages } from './loadImages';
+import { storage } from './storage';
 
-export { getRandomAnswers, shuffleArray, loadImages };
+export { getRandomAnswers, shuffleArray, loadImages, storage };
